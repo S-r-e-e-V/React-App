15ebd16d5a921cfe14cceb8029ed8a96869cb09b
Sreekanth V <25sreekanth@gmail.com>
2020-06-01 09:55:21 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import { Helmet } from 'react-helmet'
 import './App.css';
 import HomePage from './pages/HomePage'
 
 function App() {
   return (
     <div className="App">
+      <Helmet>
+      <title>App</title>
+    </Helmet>
       <div className="sideNav">
 
       </div>
